830ada3b17b7694417358dea82151b864bd721dc
John <johndbroadhead@gmail.com>
2024-08-21 19:23:41 +0100

    Add decoding for AND through CPY

--- src/instruction.rs
+++ src/instruction.rs
@@ -146,5 +146,410 @@ pub struct Instruction {
     opcode: Opcode,
     addressing_mode: AddressingMode,
     cycles: usize,
-    data: (u8, u8)
+    data: (u8, u8),
+    width: usize
+}
+
+impl Instruction {
+    pub fn decode(memory: &[u8], memory_position: u16) -> Instruction {
+        let index = memory_position as usize;
+        let opcode_byte = memory[index];
+        // We always pass the next two bytes as data as it simplifies construction logic
+        let data = (memory[index+1], memory[index+2]);
+        // Cases are in alphabetical order of opcode for readability
+        match opcode_byte {
+
+            // ADC
+            0x69 => Self {
+                opcode: Opcode::ADC,
+                addressing_mode: AddressingMode::Immediate,
+                cycles: 2,
+                width: 2,
+                data
+            },
+            0x65 => Self {
+                opcode: Opcode::ADC,
+                addressing_mode: AddressingMode::ZeroPage,
+                cycles: 3,
+                width: 2,
+                data
+            },
+            0x75 => Self {
+                opcode: Opcode::ADC,
+                addressing_mode: AddressingMode::ZeroPageIndexed,
+                cycles: 4,
+                width: 2,
+                data
+            },
+            0x6D => Self {
+                opcode: Opcode::ADC,
+                addressing_mode: AddressingMode::Absolute,
+                cycles: 4,
+                width: 3,
+                data
+            },
+            0x7D => Self {
+                opcode: Opcode::ADC,
+                addressing_mode: AddressingMode::AbsoluteIndexedX,
+                cycles: 4,
+                width: 3,
+                data
+            },
+            0x79 => Self {
+                opcode: Opcode::ADC,
+                addressing_mode: AddressingMode::AbsoluteIndexedY,
+                cycles: 4,
+                width: 3,
+                data
+            },
+            0x61 => Self {
+                opcode: Opcode::ADC,
+                addressing_mode: AddressingMode::IndexedIndirect,
+                cycles: 6,
+                width: 2,
+                data
+            },
+            0x71 => Self {
+                opcode: Opcode::ADC,
+                addressing_mode: AddressingMode::IndirectIndexed,
+                cycles: 5,
+                width: 2,
+                data
+            },
+
+            // AND
+            0x29 => Self {
+                opcode: Opcode::AND,
+                addressing_mode: AddressingMode::Immediate,
+                cycles: 2,
+                width: 2,
+                data
+            },
+            0x25 => Self {
+                opcode: Opcode::AND,
+                addressing_mode: AddressingMode::ZeroPage,
+                cycles: 3,
+                width: 2,
+                data
+            },
+            0x35 => Self {
+                opcode: Opcode::AND,
+                addressing_mode: AddressingMode::ZeroPageIndexed,
+                cycles: 4,
+                width: 2,
+                data
+            },
+            0x2D => Self {
+                opcode: Opcode::AND,
+                addressing_mode: AddressingMode::Absolute,
+                cycles: 4,
+                width: 3,
+                data
+            },
+            0x3D => Self {
+                opcode: Opcode::AND,
+                addressing_mode: AddressingMode::AbsoluteIndexedX,
+                cycles: 4,
+                width: 3,
+                data
+            },
+            0x39 => Self {
+                opcode: Opcode::AND,
+                addressing_mode: AddressingMode::AbsoluteIndexedY,
+                cycles: 4,
+                width: 3,
+                data
+            },
+            0x21 => Self {
+                opcode: Opcode::AND,
+                addressing_mode: AddressingMode::IndexedIndirect,
+                cycles: 6,
+                width: 2,
+                data
+            },
+            0x31 => Self {
+                opcode: Opcode::AND,
+                addressing_mode: AddressingMode::IndirectIndexed,
+                cycles: 5,
+                width: 2,
+                data
+            },
+
+            // ASL
+            0x0A => Self {
+                opcode: Opcode::ASL,
+                addressing_mode: AddressingMode::Accumulator,
+                width: 1,
+                cycles: 2,
+                data
+            },
+            0x06 => Self {
+                opcode: Opcode::ASL,
+                addressing_mode: AddressingMode::ZeroPage,
+                width: 2,
+                cycles: 5,
+                data
+            },
+            0x16 => Self {
+                opcode: Opcode::ASL,
+                addressing_mode: AddressingMode::ZeroPageIndexed,
+                width: 2,
+                cycles: 6,
+                data
+            },
+            0x0E => Self {
+                opcode: Opcode::ASL,
+                addressing_mode: AddressingMode::Absolute,
+                width: 3,
+                cycles: 6,
+                data
+            },
+            0x1E => Self {
+                opcode: Opcode::ASL,
+                addressing_mode: AddressingMode::AbsoluteIndexedX,
+                width: 3,
+                cycles: 7,
+                data
+            },
+
+            // BCC
+            0x90 => Self {
+                opcode: Opcode::BCC,
+                addressing_mode: AddressingMode::Relative,
+                cycles: 2,
+                width: 2,
+                data
+            },
+
+            // BCS
+            0xB0 => Self {
+                opcode: Opcode::BCS,
+                addressing_mode: AddressingMode::Relative,
+                cycles: 2,
+                width: 2,
+                data
+            },
+
+            // BEQ
+            0xF0 => Self {
+                opcode: Opcode::BEQ,
+                addressing_mode: AddressingMode::Relative,
+                cycles: 2,
+                width: 2,
+                data
+            },
+
+            // BIT
+            0x24 => Self {
+                opcode: Opcode::BIT,
+                addressing_mode: AddressingMode::ZeroPage,
+                cycles: 3,
+                width: 2,
+                data
+            },
+            0x2C => Self {
+                opcode: Opcode::BIT,
+                addressing_mode: AddressingMode::Absolute,
+                cycles: 4,
+                width: 3,
+                data
+            },
+
+            // BMI
+            0x30 => Self {
+                opcode: Opcode::BMI,
+                addressing_mode: AddressingMode::Relative,
+                cycles: 2,
+                width: 2,
+                data
+            },
+
+            // BNE
+            0xD0 => Self {
+                opcode: Opcode::BNE,
+                addressing_mode: AddressingMode::Relative,
+                cycles: 2,
+                width: 2,
+                data
+            },
+
+            // BPL
+            0x10 => Self {
+                opcode: Opcode::BPL,
+                addressing_mode: AddressingMode::Relative,
+                cycles: 2,
+                width: 2,
+                data
+            },
+
+            // BRK
+            0x00 => Self {
+                opcode: Opcode::BRK,
+                addressing_mode: AddressingMode::Implied,
+                cycles: 7,
+                width: 1,
+                data
+            },
+
+            // BVC
+            0x50 => Self {
+                opcode: Opcode::BVC,
+                addressing_mode: AddressingMode::Relative,
+                cycles: 2,
+                width: 2,
+                data
+            },
+
+            // BVS
+            0x70 => Self {
+                opcode: Opcode::BVS,
+                addressing_mode: AddressingMode::Relative,
+                cycles: 2,
+                width: 2,
+                data
+            },
+
+            // CLC
+            0x18 => Self {
+                opcode: Opcode::CLC,
+                addressing_mode: AddressingMode::Implied,
+                cycles: 2,
+                width: 1,
+                data
+            },
+
+            // CLD
+            0xD8 => Self {
+                opcode: Opcode::CLD,
+                addressing_mode: AddressingMode::Implied,
+                cycles: 2,
+                width: 1,
+                data
+            },
+
+            // CLI
+            0x58 => Self {
+                opcode: Opcode::CLI,
+                addressing_mode: AddressingMode::Implied,
+                cycles: 2,
+                width: 1,
+                data
+            },
+
+            // CLV
+            0xB8 => Self {
+                opcode: Opcode::CLV,
+                addressing_mode: AddressingMode::Implied,
+                cycles: 2,
+                width: 1,
+                data
+            },
+
+            // CMP
+            0xC9 => Self {
+                opcode: Opcode::CMP,
+                addressing_mode: AddressingMode::Immediate,
+                cycles: 2,
+                width: 2,
+                data
+            },
+            0xC5 => Self {
+                opcode: Opcode::CMP,
+                addressing_mode: AddressingMode::ZeroPage,
+                cycles: 2,
+                width: 3,
+                data
+            },
+            0xD5 => Self {
+                opcode: Opcode::CMP,
+                addressing_mode: AddressingMode::ZeroPageIndexed,
+                cycles: 2,
+                width: 4,
+                data
+            },
+            0xCD => Self {
+                opcode: Opcode::CMP,
+                addressing_mode: AddressingMode::Absolute,
+                cycles: 3,
+                width: 4,
+                data
+            },
+            0xDD => Self {
+                opcode: Opcode::CMP,
+                addressing_mode: AddressingMode::AbsoluteIndexedX,
+                cycles: 3,
+                width: 4,
+                data
+            },
+            0xD9 => Self {
+                opcode: Opcode::CMP,
+                addressing_mode: AddressingMode::AbsoluteIndexedY,
+                cycles: 3,
+                width: 4,
+                data
+            },
+            0xC1 => Self {
+                opcode: Opcode::CMP,
+                addressing_mode: AddressingMode::IndexedIndirect,
+                cycles: 2,
+                width: 6,
+                data
+            },
+            0xD1 => Self {
+                opcode: Opcode::CMP,
+                addressing_mode: AddressingMode::IndirectIndexed,
+                cycles: 2,
+                width: 5,
+                data
+            },
+
+            // CPX
+            0xE0 => Self {
+                opcode: Opcode::CPX,
+                addressing_mode: AddressingMode::Immediate,
+                width: 2,
+                cycles: 2,
+                data
+            },
+            0xE4 => Self {
+                opcode: Opcode::CPX,
+                addressing_mode: AddressingMode::ZeroPage,
+                width: 2,
+                cycles: 3,
+                data
+            },
+            0xEC => Self {
+                opcode: Opcode::CPX,
+                addressing_mode: AddressingMode::Absolute,
+                width: 2,
+                cycles: 4,
+                data
+            },
+
+            // CPY
+            0xC0 => Self {
+                opcode: Opcode::CPY,
+                addressing_mode: AddressingMode::Immediate,
+                width: 2,
+                cycles: 2,
+                data
+            },
+            0xC4 => Self {
+                opcode: Opcode::CPY,
+                addressing_mode: AddressingMode::ZeroPage,
+                width: 2,
+                cycles: 3,
+                data
+            },
+            0xCC => Self {
+                opcode: Opcode::CPY,
+                addressing_mode: AddressingMode::Absolute,
+                width: 2,
+                cycles: 4,
+                data
+            },
+
+            _ => panic!("Unsupported instruction decoded {}!", opcode_byte)
+        }
+    }
 }
